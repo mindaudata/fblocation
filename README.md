06cd053010871201cd5e36fd3c2a6fe255998596
mindaudata <mindaugas.petravicius@gmail.com>
2018-08-06 17:16:48 +0300

    quick edit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# fblocation
-facebook location map
+#Facebook location history map
+
+Download "your_location_history.html" from https://www.facebook.com/settings?tab=your_facebook_information > Download Your Information > Checkmark on Location History
+
+python tracks.py
+
+http://localhost:5050
+
+Upload the file.
--- tracks.py
+++ tracks.py
@@ -4,21 +4,21 @@ from werkzeug import secure_filename
 tracks = Flask(__name__)
 
 
-@tracks.route("/")
+@tracks.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 
-@tracks.route("/tracked", methods=['POST'])
+@tracks.route('/tracked', methods=['POST'])
 def tracked():
     if request.method == 'POST':
-        fbfile = request.files["fbfilename"]
+        fbfile = request.files['fbfilename']
         if fbfile.filename == 'your_location_history.html':
             fbfile.save(secure_filename('upload'))
             coords = get_coords('upload')
         print(coords)
         create_map(coords)
-    return render_template("tracked.html")
+    return render_template('tracked.html')
 
 
 if __name__ == '__main__':
