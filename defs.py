7ebcf79531c1f43f241f57e809e39426f0d9feec
Mindaugas Petravičius <40631615+mindaudata@users.noreply.github.com>
2018-08-06 16:52:07 +0300

    Add files via upload

---
+++ defs.py
@@ -0,0 +1,30 @@
+import re
+import pandas
+
+
+def get_coords(fb_file):
+    df = pandas.DataFrame(columns=['lat', 'lon'])
+    with open(fb_file, 'r') as fl:
+        for line in fl:
+            findings = re.findall(r'[+-]?\d+\.\d+,\s[+-]?\d+\.\d+', line)
+            for i in range(len(findings)):
+                la, lo = findings[i].split(sep=', ')
+                df = df.append({'lat': la, 'lon': lo}, ignore_index=True)
+    return df
+# print(get_coords('your_location_history.html'))
+
+
+import folium
+
+
+def create_map(df):
+    map = folium.Map(location=[54.7037527, 25.2837174],
+                     zoom_start=6, tiles='Mapbox Bright')
+    fg = folium.FeatureGroup(name='Facebook Tracked Locations')
+
+    for index, row in df.iterrows():
+        fg.add_child(folium.CircleMarker(location=[float(row['lat']), float(
+            row['lon'])], radius=5, color='#3b5998', fill=True))
+    map.add_child(fg)
+
+    map.save('templates/tracked.html')
---
+++ tracks.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+from defs import get_coords, create_map
+from werkzeug import secure_filename
+tracks = Flask(__name__)
+
+
+@tracks.route("/")
+def index():
+    return render_template("index.html")
+
+
+@tracks.route("/tracked", methods=['POST'])
+def tracked():
+    if request.method == 'POST':
+        fbfile = request.files["fbfilename"]
+        if fbfile.filename == 'your_location_history.html':
+            fbfile.save(secure_filename('upload'))
+            coords = get_coords('upload')
+        print(coords)
+        create_map(coords)
+    return render_template("tracked.html")
+
+
+if __name__ == '__main__':
+    tracks.debug = True
+    tracks.run(port=5050)
